01cf6dd131bf9c12021be7e4b5f9576b4c28814b
elifserra <e.serra.oncu@gmail.com>
2026-03-03 01:17:19 +0300

    fixed unique user & category combination

--- src/backend/app/database/migrations/create_categories.sql
+++ src/backend/app/database/migrations/create_categories.sql
@@ -4,8 +4,10 @@ CREATE TABLE categories (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name TEXT NOT NULL,
     user_id UUID NOT NULL,
-    category_type text unique not null,
+    category_type text not null,
     created_at TIMESTAMP DEFAULT NOW(),
+
+    unique (user_id, category_type),
 
     -- if the user is deleted, the category also gets deleted (cascade)
     CONSTRAINT fk_user
