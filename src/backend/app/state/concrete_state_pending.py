08c78bf8c4d565f522975776157789c3d95be61f
Alaaddin Gürsoy <alaaddin64.ag@gmail.com>
2026-03-29 20:58:13 +0300

    remove state pattern

--- src/backend/app/state/concrete_state_pending.py
+++
@@ -1,9 +0,0 @@
-from app.state.concrete_state_purchased import ConcreteStatePurchased
-from app.state.state import State
-
-class ConcreteStatePending(State):
-    def getPrice(self) -> float:
-        return 10.0 # to be replaced with actual price retrieval logic
-
-    def getStateName(self) -> str:
-        return "pending"
--- src/backend/app/state/concrete_state_purchased.py
+++
@@ -1,8 +0,0 @@
-from app.state.state import State
-
-class ConcreteStatePurchased(State):
-    def getPrice(self) -> float:
-        return 20.0 # to be replaced with actual price retrieval logic for purchased state
-
-    def getStateName(self) -> str:
-        return "purchased"
